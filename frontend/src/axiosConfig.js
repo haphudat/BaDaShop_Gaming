d7be96690cacc33169e4ef5ae97b57dc8f900e63
baopro1235 <vkdbao2k3@gmail.com>
2026-07-08 15:48:48 +0700

    fix lỗi 403 trên thiết bị vào lần đầu

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -9,4 +9,19 @@ axios.interceptors.request.use((config) => {
     return config;
 });
 
+// Tự động xử lý lỗi 401/403
+axios.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+            // Xóa thông tin cũ
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
+            // Redirect về login
+            window.location.href = "/login";
+        }
+        return Promise.reject(error);
+    }
+);
+
 export default axios;
